3dde6a41f18cee483b282779a720dcba537e8d35
Edward A Maxedon Sr <edadma@gmail.com>
2024-03-29 14:53:06 -0400

    FileSystem class

--- shared/src/main/scala/io/github/edadma/sxfs/Disk.scala
+++ shared/src/main/scala/io/github/edadma/sxfs/Disk.scala
@@ -1,5 +1,22 @@
 package io.github.edadma.sxfs
 
 abstract class Disk:
+  val blockSize: Int
+  val diskSize: Int
+
   def read(blocknum: Int, data: Array[Byte]): Unit
   def write(blocknum: Int, data: Array[Byte]): Unit
+
+class RamDisk(val blockSize: Int, val diskSize: Int) extends Disk:
+  require(blockSize > 0, "block size must be positive")
+  require(diskSize > 0, "disk size must be positive")
+
+  private val disk = new Array[Byte](blockSize * diskSize)
+
+  override def read(blocknum: Int, data: Array[Byte]): Unit =
+    require(0 <= blocknum && blocknum < diskSize, "block number out of range")
+    Array.copy(disk, blocknum * blockSize, data, 0, blockSize)
+
+  override def write(blocknum: Int, data: Array[Byte]): Unit =
+    require(0 <= blocknum && blocknum < diskSize, "block number out of range")
+    Array.copy(data, 0, disk, blocknum * blockSize, blockSize)
--- shared/src/main/scala/io/github/edadma/sxfs/FileSystem.scala
+++ shared/src/main/scala/io/github/edadma/sxfs/FileSystem.scala
@@ -1,15 +1,26 @@
 package io.github.edadma.sxfs
 
+import java.nio.ByteBuffer
+
 object FileSystem:
-  def format(disk: Disk): Boolean = false
+  def format(disk: Disk): Boolean =
+    val data = new Array[Byte](disk.blockSize)
+    val buf = ByteBuffer.wrap(data)
+
+    buf.putInt()
+  end format
+
   def check(disk: Disk): Boolean = false
 
 type Inode = Int
 
-abstract class FileSystem:
-  def create: Inode
-  def remove(inode: Inode): Boolean
-  def stat(inode: Inode): Long
+class FileSystem(disk: Disk):
+  private val data = new Array[Byte](disk.blockSize)
+  private val buf = ByteBuffer.wrap(data)
 
-  def read(inode: Inode, data: Array[Byte], length: Long, offset: Long): Int
-  def write(inode: Inode, data: Array[Byte], length: Long, offset: Long): Int
+//  def create: Inode
+//  def remove(inode: Inode): Boolean
+//  def stat(inode: Inode): Long
+//
+//  def read(inode: Inode, data: Array[Byte], length: Long, offset: Long): Int
+//  def write(inode: Inode, data: Array[Byte], length: Long, offset: Long): Int
